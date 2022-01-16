3e8fdca5bc216051500476485ee5361a871abd19
anhquyet01234 <95416287+anhquyet01234@users.noreply.github.com>
2022-01-16 15:41:34 +0700

    Add files via upload

---
+++ congdong.php
@@ -0,0 +1,234 @@
+<div class="col-md-7">
+                    <div class="wide_column">
+                        <div class=" d-flex justify-between">
+                            <p>Tất cả các cộng đồng</p>
+                            <a class="flat-button">
+                                Tạo cộng đồng
+                            </a>
+                        </div>
+                        <hr>
+                        <div class="search">
+                            <input class="TopSearch__input-mess" placeholder="Tìm kiếm" style="font-size: 13px;">
+                        </div>
+                        <hr>
+                        <div class="user-profile-box">
+                            <div class="user-profile">
+                                <img src="https://sun6-21.userapi.com/s/v1/if1/fESBs-Lu5jOKDBGL0VRYesAAMLWFeYgTnxDRROpPIFmxcp3Yv5ExONtJ9pTkuiRaKZvTtHLj.jpg?size=100x100&quality=96&crop=31,31,897,897&ava=1" alt="">
+                                <div>
+                                    <p>Netflix</p>
+                                    <p><small>Phim, bộ</small></p>
+                                    <small>1,971,205 người đăng ký</small>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="more_horizontal_24__Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="more_horizontal_24__more_horizontal_24"><path id="more_horizontal_24__Bounds" d="M24 0H0v24h24z"></path><path d="M18 10a2 2 0 012 2 2 2 0 01-2 2 2 2 0 01-2-2c0-1.1.9-2 2-2zm-6 4a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2zm-6 0a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2z" id="more_horizontal_24__Mask" fill="currentColor"></path></g></g>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="user-profile-box">
+                            <div class="user-profile">
+                                <img src="https://sun6-22.userapi.com/s/v1/ig2/akZ5V3FmhjCJOnivA7VEaF2Qplrw5xPvR0Ypf9dn1X3_5hlRAe-Nd7c-yqHse1p6qOiImoPMBjmkDgGuyfPcS9mA.jpg?size=100x100&quality=96&crop=0,0,900,900&ava=1" alt="">
+                                <div>
+                                    <p>Realme</p>
+                                    <p><small>Phim, bộ</small></p>
+                                    <small>1,971,205 người đăng ký</small>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="more_horizontal_24__Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="more_horizontal_24__more_horizontal_24"><path id="more_horizontal_24__Bounds" d="M24 0H0v24h24z"></path><path d="M18 10a2 2 0 012 2 2 2 0 01-2 2 2 2 0 01-2-2c0-1.1.9-2 2-2zm-6 4a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2zm-6 0a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2z" id="more_horizontal_24__Mask" fill="currentColor"></path></g></g>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="user-profile-box">
+                            <div class="user-profile">
+                                <img src="https://sun6-22.userapi.com/s/v1/ig2/akZ5V3FmhjCJOnivA7VEaF2Qplrw5xPvR0Ypf9dn1X3_5hlRAe-Nd7c-yqHse1p6qOiImoPMBjmkDgGuyfPcS9mA.jpg?size=100x100&quality=96&crop=0,0,900,900&ava=1" alt="">
+                                <div>
+                                    <p>Realme</p>
+                                    <p><small>Phim, bộ</small></p>
+                                    <small>1,971,205 người đăng ký</small>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="more_horizontal_24__Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="more_horizontal_24__more_horizontal_24"><path id="more_horizontal_24__Bounds" d="M24 0H0v24h24z"></path><path d="M18 10a2 2 0 012 2 2 2 0 01-2 2 2 2 0 01-2-2c0-1.1.9-2 2-2zm-6 4a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2zm-6 0a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2z" id="more_horizontal_24__Mask" fill="currentColor"></path></g></g>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="user-profile-box">
+                            <div class="user-profile">
+                                <img src="https://sun6-22.userapi.com/s/v1/ig2/akZ5V3FmhjCJOnivA7VEaF2Qplrw5xPvR0Ypf9dn1X3_5hlRAe-Nd7c-yqHse1p6qOiImoPMBjmkDgGuyfPcS9mA.jpg?size=100x100&quality=96&crop=0,0,900,900&ava=1" alt="">
+                                <div>
+                                    <p>Realme</p>
+                                    <p><small>Phim, bộ</small></p>
+                                    <small>1,971,205 người đăng ký</small>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="more_horizontal_24__Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="more_horizontal_24__more_horizontal_24"><path id="more_horizontal_24__Bounds" d="M24 0H0v24h24z"></path><path d="M18 10a2 2 0 012 2 2 2 0 01-2 2 2 2 0 01-2-2c0-1.1.9-2 2-2zm-6 4a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2zm-6 0a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2z" id="more_horizontal_24__Mask" fill="currentColor"></path></g></g>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="user-profile-box">
+                            <div class="user-profile">
+                                <img src="https://sun6-22.userapi.com/s/v1/ig2/akZ5V3FmhjCJOnivA7VEaF2Qplrw5xPvR0Ypf9dn1X3_5hlRAe-Nd7c-yqHse1p6qOiImoPMBjmkDgGuyfPcS9mA.jpg?size=100x100&quality=96&crop=0,0,900,900&ava=1" alt="">
+                                <div>
+                                    <p>Realme</p>
+                                    <p><small>Phim, bộ</small></p>
+                                    <small>1,971,205 người đăng ký</small>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="more_horizontal_24__Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="more_horizontal_24__more_horizontal_24"><path id="more_horizontal_24__Bounds" d="M24 0H0v24h24z"></path><path d="M18 10a2 2 0 012 2 2 2 0 01-2 2 2 2 0 01-2-2c0-1.1.9-2 2-2zm-6 4a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2zm-6 0a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2z" id="more_horizontal_24__Mask" fill="currentColor"></path></g></g>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="user-profile-box">
+                            <div class="user-profile">
+                                <img src="https://sun6-22.userapi.com/s/v1/ig2/akZ5V3FmhjCJOnivA7VEaF2Qplrw5xPvR0Ypf9dn1X3_5hlRAe-Nd7c-yqHse1p6qOiImoPMBjmkDgGuyfPcS9mA.jpg?size=100x100&quality=96&crop=0,0,900,900&ava=1" alt="">
+                                <div>
+                                    <p>Realme</p>
+                                    <p><small>Phim, bộ</small></p>
+                                    <small>1,971,205 người đăng ký</small>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="more_horizontal_24__Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="more_horizontal_24__more_horizontal_24"><path id="more_horizontal_24__Bounds" d="M24 0H0v24h24z"></path><path d="M18 10a2 2 0 012 2 2 2 0 01-2 2 2 2 0 01-2-2c0-1.1.9-2 2-2zm-6 4a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2zm-6 0a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2z" id="more_horizontal_24__Mask" fill="currentColor"></path></g></g>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="user-profile-box">
+                            <div class="user-profile">
+                                <img src="https://sun6-22.userapi.com/s/v1/ig2/akZ5V3FmhjCJOnivA7VEaF2Qplrw5xPvR0Ypf9dn1X3_5hlRAe-Nd7c-yqHse1p6qOiImoPMBjmkDgGuyfPcS9mA.jpg?size=100x100&quality=96&crop=0,0,900,900&ava=1" alt="">
+                                <div>
+                                    <p>Realme</p>
+                                    <p><small>Phim, bộ</small></p>
+                                    <small>1,971,205 người đăng ký</small>
+                                </div>
+                            </div>
+                            <div>
+                                <a href="#">
+                                    <span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="more_horizontal_24__Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="more_horizontal_24__more_horizontal_24"><path id="more_horizontal_24__Bounds" d="M24 0H0v24h24z"></path><path d="M18 10a2 2 0 012 2 2 2 0 01-2 2 2 2 0 01-2-2c0-1.1.9-2 2-2zm-6 4a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2zm-6 0a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2z" id="more_horizontal_24__Mask" fill="currentColor"></path></g></g>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 right-sidebar">
+                    <div class="contentRight">
+                        <div class="side_bar_nav">
+                            <ol class="side_bar_ol">
+                                <li>
+                                    <a href="">
+                                        <div class="LeftMenu__icon">
+                                            <span class="left_label inl_bl">Cộng đồng của tôi</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <div class="LeftMenu__icon">
+                                            <span class="left_label inl_bl">các sự kiện</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <div class="LeftMenu__icon">
+                                            <span class="left_label inl_bl">Tìm kiếm cộng đồng</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <div class="LeftMenu__icon">
+                                            <span class="left_label inl_bl">Bạn bè giới thiệu</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <div class="LeftMenu__icon">
+                                            <span class="left_label inl_bl">Khuyến nghị</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <hr>
+                                <li>
+                                    <a href="">
+                                        <div class="LeftMenu__icon">
+                                            <span class="left_label inl_bl">Cộng đồng phổ biến</span>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                    <div class="contentRight mt-3">
+                        <div class="side_bar_nav">
+                            <ol class="side_bar_ol">
+                                <li>
+                                    <a href="" class="d-flex justify-between">
+                                        <div class="LeftMenu__icon">
+                                            <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6 8.9a.9.9 0 00.23-.03 2.9 2.9 0 005.54 0 .9.9 0 00.23.03h8a.9.9 0 100-1.8h-8a.9.9 0 00-.23.03 2.9 2.9 0 00-5.54 0A.9.9 0 006 7.1H4a.9.9 0 100 1.8h2zm3-2a1.1 1.1 0 100 2.2 1.1 1.1 0 000-2.2zM3.1 16a.9.9 0 01.9-.9h8a.9.9 0 01.23.03 2.9 2.9 0 015.54 0 .9.9 0 01.23-.03h2a.9.9 0 110 1.8h-2a.9.9 0 01-.23-.03 2.9 2.9 0 01-5.54 0 .9.9 0 01-.23.03H4a.9.9 0 01-.9-.9zm10.8 0a1.1 1.1 0 112.2 0 1.1 1.1 0 01-2.2 0z" fill="currentColor"></path>
+                                            </svg>
+                                            <span class="left_label inl_bl">Thú vị nhất</span>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                    <div class="contentRight mt-3">
+                        <div class="d-flex">
+                            <img style="width: 34px; height: 34px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrlDZQZEHFUA-wNA-YdBDe3nZWsThMI2HSVQ&usqp=CAU" style="width: 50px; height: 50px; border-radius: 50%;">
+                            <div class="ml-2">
+                                <p style="margin-bottom: 0;">
+                                    <b>Mời bạn bè</b>
+                                </p>
+                                <span style="color: #818c99; font-size: 14px;">Mời theo số điện thoại hoặc email</span>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-2">
+                            <img style="width: 34px; height: 34px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNdd11HWoFKOaGvr-bFsPE78aupBUko0loNA&usqp=CAU" style="width: 50px; height: 50px; border-radius: 50%;">
+                            <div class="ml-2">
+                                <p style="margin-bottom: 0;">
+                                    <b>Google</b>
+                                </p>
+                                <span style="color: #818c99; font-size: 14px;">Tìm bạn qua dịch vụ Google</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
---
+++ messger.php
@@ -0,0 +1,70 @@
+
+                <div class="col-md-7">
+                    <div class="wide_column">
+                        <div class="d-flex justify-between">
+                            <div>
+                                <div class="search">
+                                    <input class="TopSearch__input-mess" placeholder="Tìm kiếm" style="font-size: 13px;">
+                                </div>
+                            </div>
+                            <div>
+                                <span class="ml-2 TopNavBtn__icon">
+                                    <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><path d="M4.5 4.12c-2.93 3.22-1.37 8.19 2.92 12.5 4.2 4.21 9.4 5.8 12.47 2.93 1.68-1.57 1.44-4.34-.64-6.19-1.88-1.6-4.83-1.87-6.72-.1-.63-.54-1.17-1.17-1.75-1.76 1.63-1.7 1.62-4.68-.06-6.68-1.88-2.25-4.74-2.33-6.22-.7zm4.85 1.85c1.05 1.27 1.2 3.29-.05 4.46-.63.57-1.25.9 1.03 3.22 2.45 2.5 2.76 1.62 3.4.95 1-1.01 2.97-1.1 4.32.1 1.5 1.33 1.33 2.87.62 3.53-2.22 2.07-6.47.64-9.97-2.88-3.5-3.52-5.06-7.61-2.87-10.02.5-.56 2.1-1.05 3.52.64zM18.5 2c.5 0 .9.4.9.9v1.7h1.7a.9.9 0 010 1.8h-1.7v1.7a.9.9 0 11-1.8 0V6.4h-1.7a.9.9 0 010-1.8h1.7V2.9c0-.5.4-.9.9-.9z"></path></g>
+                                    </svg>
+                                </span>
+                                <span class="ml-2 TopNavBtn__icon">
+                                    <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill="currentColor" fill-rule="evenodd"><path d="M9.56 4.1h3.54a.9.9 0 110 1.8H9.6c-1 0-1.69 0-2.23.04-.52.05-.82.13-1.05.24a2.6 2.6 0 00-1.14 1.14c-.11.23-.2.53-.24 1.05-.04.54-.04 1.24-.04 2.23v3.8c0 1 0 1.69.04 2.23.05.52.13.82.24 1.05.25.49.65.89 1.14 1.14.23.11.53.2 1.05.24.54.04 1.24.04 2.23.04h3.8c1 0 1.69 0 2.23-.04.52-.05.82-.13 1.05-.24a2.6 2.6 0 001.14-1.14c.11-.23.2-.53.24-1.05.04-.54.04-1.24.04-2.23v-3.5a.9.9 0 011.8 0v3.54c0 .95 0 1.71-.05 2.33a4.5 4.5 0 01-.43 1.73 4.4 4.4 0 01-1.92 1.92 4.5 4.5 0 01-1.73.43c-.62.05-1.38.05-2.33.05H9.56c-.95 0-1.71 0-2.33-.05a4.5 4.5 0 01-1.73-.43 4.4 4.4 0 01-1.92-1.92 4.51 4.51 0 01-.43-1.73c-.05-.62-.05-1.38-.05-2.33v-3.88c0-.95 0-1.71.05-2.33.05-.64.16-1.2.43-1.73A4.4 4.4 0 015.5 4.58a4.51 4.51 0 011.73-.43c.62-.05 1.38-.05 2.33-.05z"></path><path d="M19.12 3.33a1.1 1.1 0 111.56 1.55l-.35.35a.4.4 0 01-.57 0l-.99-.99a.4.4 0 010-.56zm-.6 2.57l-.42-.42c-.44-.44-.72-.42-1.13 0l-5.13 5.12c-1.95 1.96-3.19 3.89-2.76 4.32.43.43 2.36-.8 4.32-2.76l5.12-5.13c.44-.44.42-.72 0-1.13z"></path></g>
+                                </svg>
+                                </span>
+                                <span class="ml-2 TopNavBtn__icon">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="more_horizontal_24__Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="more_horizontal_24__more_horizontal_24"><path id="more_horizontal_24__Bounds" d="M24 0H0v24h24z"></path><path d="M18 10a2 2 0 012 2 2 2 0 01-2 2 2 2 0 01-2-2c0-1.1.9-2 2-2zm-6 4a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2zm-6 0a2 2 0 01-2-2c0-1.1.9-2 2-2a2 2 0 012 2 2 2 0 01-2 2z" id="more_horizontal_24__Mask" fill="currentColor"></path></g></g>
+                                    </svg>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="messger mt-3 pt-3" style="height: 100vh; border-top: 1px solid #e7e8ec;">
+                            <div class="d-flex">
+                                <img src="asset/images/anh.png" style="width: 50px; height: 50px; border-radius: 50%;">
+                                <div class="ml-3">
+                                    <p style="margin-bottom: 0;">
+                                        <b>ВКонтакте</b>
+                                    </p>
+                                    <span style="color: #818c99; font-size: 14px;">Xin chào Đ! Ở đây bạn có thể thư từ, gọi điện, gửi hình ảnh, âm nhạc và thậm chí cả tiền.</span>
+                                </div>
+                            </div>
+                            <div class="ConvoListFindFriendsStub text-center">
+                                <p style="margin-bottom: 0;">Nhìn xem ai ở đây</p>
+                                <span style="color: #818c99; font-size: 14px;">Tìm xem ai trong số những người bạn của bạn đã tham gia VKontakte</span>
+                                <div class="mt-2">
+                                    <a class="flat-button">
+                                        Tìm bạn
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-md-3 right-sidebar">
+                    <div class="contentRight">
+                        <div class="side_bar_nav">
+                            <ol class="side_bar_ol">
+                                <li>
+                                    <a href="">
+                                        <div class="LeftMenu__icon">
+                                            <span class="left_label inl_bl">Tất cả chat</span>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="">
+                                        <div class="LeftMenu__icon">
+                                            <span class="left_label inl_bl">Chưa đọc</span>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
