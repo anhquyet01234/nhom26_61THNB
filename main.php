a72f28e1e5b847688028d0182fde690484d2fdf5
Cuong0211 <86772239+Cuong0211@users.noreply.github.com>
2022-01-16 16:42:45 +0700

    Update main.php

--- main.php
+++ main.php
@@ -145,7 +145,7 @@ if ($result->num_rows > 0) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Facebook</title>
+    <title>VK.com</title>
     <link rel="shortcut icon" type="image/gif" href="asset/images/anh.png">
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
