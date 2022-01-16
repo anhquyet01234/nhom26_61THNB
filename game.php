e0060773af398c52e8d57cd3fb04f1e240b65111
anhquyet01234 <95416287+anhquyet01234@users.noreply.github.com>
2022-01-16 09:53:53 +0700

    Add files via upload

---
+++ game.php
@@ -0,0 +1,41 @@
+<div class="col-md-10">
+                    <div class="wide_column">
+                        <div class="search">
+                            <input class="TopSearch__input-mess" placeholder="Tìm kiếm" style="font-size: 13px;">
+                        </div>
+                    </div>
+                    <div class="wide_column mt-3">
+                        <div class="video">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <img width="100%" src="https://sun6-22.userapi.com/impf/Oo-jPuzJjVelisdTAXNN6FHF0h2AmJCv3j2Saw/ZBdC_QQfdeo.jpg?size=139x139&quality=90&sign=b9b886f1f81705b13b0a73e207c897cc">
+                                    <div class="selfContent mt-2">
+                                        <p class="mb-1"><b>Game</b></p>
+                                        <p>Собери слаймы три в ряд!</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <img width="100%" src="https://sun6-22.userapi.com/impf/MPo_eKoMx9MsGmPjJmCr9MAMOwphVKmkD16BRw/Rvvl2jtj5C8.jpg?size=139x139&quality=90&sign=1166a4cb34ded17fda18de36002e7c31">
+                                    <div class="selfContent mt-2">
+                                        <p class="mb-1"><b>Game</b></p>
+                                        <p>Собери слаймы три в ряд!</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <img width="100%" src="https://sun6-20.userapi.com/impf/seXfdkOBKq_W0rLwlYXaR2EJpjVvcFXJfB05vA/bdzak67KdgA.jpg?size=139x139&quality=90&sign=6be7f1b77156bb8f9d853201919de5d2">
+                                    <div class="selfContent mt-2">
+                                        <p class="mb-1"><b>Game</b></p>
+                                        <p>Собери слаймы три в ряд!</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <img width="100%" src="https://sun6-20.userapi.com/impf/0L5zFvitJ2Ctcsyhdt61OZ8UM5q-Nz-2lqUmCw/mEJR7MbwWbo.jpg?size=139x139&quality=90&sign=05a5944c1338785fa2b987cc0772ef99">
+                                    <div class="selfContent mt-2">
+                                        <p class="mb-1"><b>Game</b></p>
+                                        <p>Собери слаймы три в ряд!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
